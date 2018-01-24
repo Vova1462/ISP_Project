9e6ccd76110d0f84372a181184c5a6e6da927633
Vladimir <Sencor@bk.ru>
2018-01-24 17:06:25 +0300

    Add random

--- Assets/Script/Barier.cs
+++ Assets/Script/Barier.cs
@@ -2,15 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class Barier : MonoBehaviour {
-
+    public float speed = 500f;
 	// Use this for initialization
-	void Start () {
-		
-	}
-	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+       GameObject target = GameObject.FindGameObjectWithTag("Player");
+       if (target != null)
+        {
+            this.GetComponent<Rigidbody>().velocity = this.transform.right * speed;
+            if (this.transform.position.x <= -22f)
+            {
+                Destroy(gameObject);
+            }
+     
+        }
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            Destroy(collision.gameObject);
+            collision.gameObject.tag = "Dead";
+        }
+    }
 }
--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -12,18 +12,37 @@ public class Player : MonoBehaviour {
     public float handRange = 30f;
     public Transform raySpawn;
     private Camera fpsCam;
-    private float catchRate;
+    private float catchRate=0.2f;
     private WaitForSeconds catchDuration = new WaitForSeconds(.07f);
     private LineRenderer laserLine;
-    private float delay;
+    public float last_time = 0f;
+    public float delta_time_bar = 1f;
+    public float delay = 1;
+
+    public Transform barier_spawn;
+    public GameObject barier_prefab;
     // Use this for initialization
     void Start () {
         laserLine = GetComponent<LineRenderer>();
-        fpsCam = GetComponentInParent<Camera>();
+        fpsCam = GetComponentInChildren<Camera>();
+        playerRigidbody = GetComponent<Rigidbody>();
+        CreateBarier();
     }
 
     // Update is called once per frame
     void Update () {
+        Vector3 input = new Vector3(0, 0, Input.GetAxisRaw("Horizontal"));
+        Vector3 velocity = input.normalized * player_speed;
+        Vector3 movePosition = velocity * Time.deltaTime;
+
+        transform.position += movePosition;
+        if (Time.time - last_time >= delta_time_bar)
+        {
+            CreateBarier();
+            last_time = Time.time;
+            print("Train");
+        }
+            
         if (Input.GetButton("Fire1") && Time.time > delay)
         {
             StartCoroutine(CatchEffect());
@@ -39,8 +58,28 @@ public class Player : MonoBehaviour {
             }
             delay = Time.time + catchRate;
         }
-		
 	}
+
+    public void CreateBarier()
+    {
+        float z = 0;
+        int position = Random.Range(1, 3);
+        if (position == 1)
+            z = -3.5f;
+        else if (position == 2)
+            z = 0f;
+        else
+            z = 3.5f;
+        barier_spawn.position = new Vector3(22, 0.0f, z);
+        GameObject train = (GameObject)Instantiate(barier_prefab, barier_spawn.position, barier_spawn.rotation);
+        //print("Done");
+    }
+
+    /*public bool isReadyToCreate()
+    {
+        return (Time.time >= last_time + delay);
+    }*/
+
     private IEnumerator CatchEffect()
     {
         laserLine.enabled = true;
